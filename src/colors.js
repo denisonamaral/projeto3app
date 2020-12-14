63accfac855c98c87e645ecb339efeb123386b37
Hector Lima <hectoraugusto12@yahoo.ie>
2020-12-14 00:34:15 +0000

    denis nav

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+export const silver = {
+  bgColor1: "rgba(91, 100, 103, 0.1)",
+  bgColor2: "rgba(47, 67, 83, 0.8)",
+  fontColor: "rgba(176, 176, 176)",
+  fontColorSelected: "rgba(255, 255, 255)",
+  dividerColor: "rgba(94, 104, 107)",
+  selectedBackgroundCollapsedMode: "white",
+};
---
+++ src/components/MainView/Destinations/Country/Country.js
@@ -0,0 +1,45 @@
+import React from "react";
+import * as s from "./Country.styles";
+
+const Country = (props) => {
+  const country = props.match.params.country;
+  const countries = {
+    amsterdam: {
+      description: "Conteúdo aqui",
+    },
+    berlin: {
+      description: "Conteúdo aqui",
+    },
+    madrid: {
+      description: "Conteúdo aqui",
+    },
+    miami: {
+      description: "Conteúdo aqui",
+    },
+    saopaulo: {
+      description: "Conteúdo aqui",
+    },
+    barcelona: {
+      description: "Conteúdo aqui",
+    },
+    lisbon: {
+      description: "Conteúdo aqui",
+    },
+    mexico: {
+      description: "Conteúdo aqui",
+    },
+    paris: {
+      description: "Conteúdo aqui",
+    },
+  };
+  return (
+    <s.CountryContainer>
+      <s.CountryImage img={countries[country]["img"]} />
+      <s.CountryDescription>
+        {countries[country]["description"]}
+      </s.CountryDescription>
+    </s.CountryContainer>
+  );
+};
+
+export default Country;
