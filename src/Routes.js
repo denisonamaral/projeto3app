0f853f56c57c4e248c49595ae62676a8f8c58c2f
denisonamaral <denison.amaral@outlook.com>
2020-12-14 15:47:02 -0300

    bug profile

--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 // Components
-import Home from './components/MainView/Home/Home';
-import About from './components/MainView/About/About';
-import Destinations from './components/MainView/Destinations/Destinations';
-import Country from './components/MainView/Destinations/Country/Country'
-import Blog from './components/MainView/Blog/Blog';
-import Services from './components/MainView/Services/Services';
-import Contacts from './components/MainView/Contacts/Contacts';
+import Home from "./components/MainView/Home/Home";
+import About from "./components/MainView/About/About";
+import Destinations from "./components/MainView/Destinations/Destinations";
+import Country from "./components/MainView/Destinations/Country/Country";
+import Blog from "./components/MainView/Blog/Blog";
+import Services from "./components/MainView/Services/Services";
+import Contacts from "./components/MainView/Contacts/Contacts";
+import Profile from "./components/profile/Profile";
+import Login from "./routeComponents/auth/Login";
+import Signup from "./routeComponents/auth/Signup";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/destinations' component={Destinations} />
-      <Route exact path='/destinations/:country' component={Country} />
-      <Route exact path='/blog' component={Blog} />
-      <Route exact path='/services' component={Services} />
-      <Route exact path='/contacts' component={Contacts} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/destinations" component={Destinations} />
+      <Route exact path="/destinations/:country" component={Country} />
+      <Route exact path="/blog" component={Blog} />
+      <Route exact path="/services" component={Services} />
+      <Route exact path="/contacts" component={Contacts} />
+      <Route exact path="/meuperfil" component={Profile} />
+      <Route exact path="/signup" component={Login} />
+      <Route exact path="/login" component={Signup} />
     </Switch>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
